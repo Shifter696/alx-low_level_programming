28d1062d710ff44627835b604c143d29fa9bf63d
Shifter696 <kkalu.gk@gmail.com>
2022-12-08 13:21:34 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,21 @@
+/*
+ *File: 4-puts.c
+ *Auth: Shifter696
+ */
+
 #include <stdio.h>
 
-int main (void)
-{
-	puts ("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-}
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle,following a new line*"
+ *
+ * Return : Always 0.
+ */
+ int main(void)
+ {
+ 
+ 		puts("\"Programming is like building a multilingual puzzle");
+ 		
+ 			return (0);
+ 
+ }
