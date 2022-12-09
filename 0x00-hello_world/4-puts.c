cea5192a1feef37371c4fa18e3e499c51189dea0
Shifter696 <kkalu.gk@gmail.com>
2022-12-09 08:01:53 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,23 @@
-	/*
-	 * File: 4-puts.c
-	 *Auth: Shifter696
-	 */
-	
-	#include <stdio.h>
-	
-	/**
-	 *  main - Prints "Programming is like building a multilingual
-	 *  			puzzle, followed by a new line."
-	 *
-	 *  Return: Always 0.
-	 * /
-		int main(void)
-		
-		{
-		
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
 		puts("\"Programming is like building a multilingual puzzle");
-		
-		
-		
-		return (0);
-		
-		}
+
+			return (0);
+
+}
