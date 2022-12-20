bc2bc59d430fc8a93c9d5d667e65022b21001fb0
Shifter696 <kkalu.gk@gmail.com>
2022-12-20 11:28:10 -0800

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,13 @@
-#include 'main.h'
+#include "main.h"
+/**
+ * print alphabet - print all alphabets in lowercase
+ */
+void print_alphabet_x10(void);
+{
+		char letter;
+
+		for(letter = 'a'; letter <='z'; letter++)
+			_putchar(letter);
+		
+		_putchar('\n')
+}
